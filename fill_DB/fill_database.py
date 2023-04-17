98941323d563030cb1d80a733957962d3c1f08bc
DSulzhits <dsulzhits@gmail.com>
2023-04-17 17:03:11 +0300

    Написан код для заполнения базы данных

--- fill_DB/fill_database.py
+++ fill_DB/fill_database.py
@@ -1,23 +1,38 @@
 from classes.engine_HH import HH
 import psycopg2
 import os
-import csv
 from dotenv import load_dotenv
 
-load_dotenv()
-postgres_key = os.getenv('POSTGRESSQL_KEY')
 
+def fill_employers_db(employers_list):
+    employers_id = []
+    load_dotenv()
+    postgres_key = os.getenv('POSTGRESSQL_KEY')
+    for employer in employers_list:
+        employer_info = HH(employer)
+        employer_info.get_employer()
+    employers = HH.employers_data
 
-def fill_DB(employer):
-    employer = HH(employer)
-    employer_data = employer.get_request()
-    employer_vacancies = employer_data.get_vacancies()
     conn = psycopg2.connect(host='localhost', database='employers_db', user='postgres', password=postgres_key)
     try:
-        with conn.cursor() as cur:
-            for vacancy in range(len(employer_vacancies)):
-                cur.execute(f'INSERT INTO  {employer}  VALUES (%, %, %, %, %, %)', (vacancy['employer']))
-
-
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM employers")
+                rows = cur.fetchall()
+                for row in rows:
+                    emp_id, name, url = row
+                    employers_id.append(emp_id)
+                for employer in employers:
+                    if int(employer['id']) not in employers_id:
+                        cur.execute('INSERT INTO employers VALUES (%s, %s, %s)',
+                                    (employer['id'],
+                                     employer['name'],
+                                     employer['alternate_url']))
+                    else:
+                        print(f"Работодатель {employer['name']} id {employer['id']} уже существует")
     finally:
         conn.close()
+
+#
+# fill_DB(['skyeng', 'skillbox', 'лаборатория касперского', 'lesta games', 'VK', 'LG Electronics Inc.',
+#          'SberTech', 'YADRO', 'Доктор Веб', 'GeekBrains'])

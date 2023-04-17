cf4c933201fef8f9cf0814f0d61d8ce4f83d7e74
DSulzhits <dsulzhits@gmail.com>
2023-04-17 17:03:27 +0300

    Убрано лишнее в main

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from classes.engine_HH import HH
+from fill_DB.fill_database import fill_employers_db
 
 
 def main():
     employers = ['skyeng', 'skillbox', 'лаборатория касперского', 'lesta games', 'VK', 'LG Electronics Inc.',
                  'SberTech', 'YADRO', 'Доктор Веб', 'GeekBrains']
-    for employer in employers:
-        employer_info = HH(employer)
-        employer_info.get_employer()
-        for vacancy_dict in HH.employers_data:
-            vacancies = employer_info.get_vacancies(vacancy_dict['id'])
-            for vacancy in vacancies:
-                print(vacancy)
-        break
+    fill_employers_db(employers)
 
 
 if __name__ == "__main__":

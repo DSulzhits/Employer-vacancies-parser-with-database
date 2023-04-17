10755a4abea7df749b306bd2e522825c7853da12
DSulzhits <dsulzhits@gmail.com>
2023-04-17 14:49:26 +0300

    Скорректированы методы исходя из схемы построения будущих таблиц

--- classes/engine_HH.py
+++ classes/engine_HH.py
@@ -4,37 +4,48 @@ import requests
 
 class HH:
     """Класс для доступа к API HeadHunter"""
-    employer_data = {}
+    employer_dict = {}
+    employers_data = []
     vacancies_emp = []
     vacancies_emp_dicts = []
 
     def __init__(self, employer):
         self.employer = employer
 
-    def get_request(self):
-        """Метод для отправки запроса на HeadHunter проводит необходимые проверки,
-        записывает полученную информацию в .json файл,
-        возвращает словари для последующей работы с ними.
-        """
+    def get_employer(self):
+        url = 'https://api.hh.ru/employers'
+        params = {'text': {self.employer}, "areas": 113, 'per_page': 20}
+        response = requests.get(url, params=params)
+        employer = response.json()
+        if employer is None:
+            return "Данные не получены"
+        elif 'items' not in employer:
+            return "Нет указанных работодателей"
+        else:
+            employer_dict = {'id': employer['items'][0]['id'], 'name': employer['items'][0]['name'],
+                             'alternate_url': employer['items'][0]['alternate_url']}
+            self.employer_dict = employer_dict
+            self.employers_data.append(employer_dict)
+            return self.employer_dict
 
-        for num in range(
-                50):  # при значении 50 выбирает из 1000 вакансий те в которых есть информация о З/П и она в RUR
-            url = 'https://api.hh.ru/employers'
-            params = {'text': {self.employer}, "areas": 113, 'per_page': 20}
-            response = requests.get(url, params=params)
-            employer = response.json()
-            if employer is None:
-                return "Данные не получены"
-            elif 'items' not in employer:
-                return "Нет указанных работодателей"
-            else:
-                self.employer_data['id'] = employer['items'][0]['id']
-                self.employer_data['name'] = employer['items'][0]['name']
-                self.employer_data['alternate_url'] = employer['items'][0]['alternate_url']
-            return self.employer_data
+        # for num in range(
+        #         50):
+        #     url = 'https://api.hh.ru/employers'
+        #     params = {'text': {self.employer}, "areas": 113, 'per_page': 20}
+        #     response = requests.get(url, params=params)
+        #     employer = response.json()
+        #     if employer is None:
+        #         return "Данные не получены"
+        #     elif 'items' not in employer:
+        #         return "Нет указанных работодателей"
+        #     else:
+        #         self.employer_data['id'] = employer['items'][0]['id']
+        #         self.employer_data['name'] = employer['items'][0]['name']
+        #         self.employer_data['alternate_url'] = employer['items'][0]['alternate_url']
+        #     return self.employer_data
 
-    def get_page_vacancies(self, page):
-        employer_id = self.employer_data['id']
+    def __get_page_vacancies(self, employer_id, page):
+        self.employer_id = employer_id
         params = {
             'employer_id': employer_id,
             'area': 113,
@@ -46,30 +57,33 @@ class HH:
         response.close()
         return data
 
-    def get_vacancies(self):
+    def get_vacancies(self, employer_id):
         for page in range(2):
-            vacancies = json.loads(self.get_page_vacancies(page))
+            vacancies = json.loads(self.__get_page_vacancies(employer_id, page))
             for vacancy in vacancies['items']:
                 if vacancy['salary'] is None:
                     vacancy['salary'] = {}
                     vacancy['salary']['from'] = "не указано"
                     vacancy['salary']['to'] = "не указано"
                 self.vacancies_emp.append(
-                    [vacancy['name'], vacancy['apply_alternate_url'], vacancy['snippet']['requirement'],
+                    [vacancy['id'], vacancy['name'], vacancy['apply_alternate_url'],
                      vacancy['salary']['from'], vacancy['salary']['to']])
 
         for vacancy in self.vacancies_emp:
-            vacancy_dict = {'employer': self.employer_data['name'], 'vacancy': vacancy[0], 'url': vacancy[1],
-                            'requirement': vacancy[2], 'salary_from': vacancy[3], 'salary_to': vacancy[4]}
+            vacancy_dict = {'id': vacancy[0], 'vacancy': vacancy[1], 'url': vacancy[2],
+                            'salary_from': vacancy[3], 'salary_to': vacancy[4],
+                            'employer': self.employer_dict['id']}
             if vacancy_dict['salary_from'] is None:
                 vacancy_dict['salary_from'] = 0
             elif vacancy_dict['salary_to'] is None:
                 vacancy_dict['salary_to'] = vacancy_dict['salary_from']
-                try:
-                    if "<highlighttext>" and "</highlighttext>" in vacancy_dict['requirement']:
-                        vacancy_dict['requirement'] = vacancy_dict['requirement'].replace("<highlighttext>", "")
-                        vacancy_dict['requirement'] = vacancy_dict['requirement'].replace("</highlighttext>", "")
-                except TypeError:
-                    vacancy_dict['requirement'] = vacancy_dict['requirement']
             self.vacancies_emp_dicts.append(vacancy_dict)
         return self.vacancies_emp_dicts
+
+#
+# hh = HH('Skyeng')
+# emp = hh.get_employer()
+# print(emp)
+# vacancies = hh.get_vacancies(emp['id'])
+# for vacancy in vacancies:
+#     print(vacancy)

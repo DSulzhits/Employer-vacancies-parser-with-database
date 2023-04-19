e6d0750fea1e74ae7cc564cf09c1315cef9306f8
DSulzhits <dsulzhits@gmail.com>
2023-04-19 22:13:14 +0300

    Переписан main под работу с классами

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fill_DB.fill_database import fill_employers_db
+from fill_DB.fill_database import FillDB
 
 
 def main():
-    employers = ['skyeng', 'skillbox', 'лаборатория касперского', 'lesta games', 'VK', 'LG Electronics Inc.',
+    employers = ['skyeng', 'skillbox', 'лаборатория касперского', 'lesta games', 'Вконтакте', 'LG Electronics Inc.',
                  'SberTech', 'YADRO', 'Доктор Веб', 'GeekBrains']
-    fill_employers_db(employers)
+    fill_db = FillDB(employers)
+    fill_db.fill_db_employers()
+    fill_db.fill_db_vacancies()
 
 
 if __name__ == "__main__":

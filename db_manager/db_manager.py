6edd67b638575da11965bf4e18fe5f93496009f9
DSulzhits <dsulzhits@gmail.com>
2023-04-19 22:12:10 +0300

    Добавлена проверка

--- db_manager/db_manager.py
+++ db_manager/db_manager.py
@@ -26,26 +26,26 @@ class DBManager:
             conn.close()
         return vacancies
 
-    # @classmethod
-    # def get_companies_and_vacancies_count(cls):
-    #     employers_list = []
-    #     for company in cls.get_info_employers():
-    #         vacancies_list = []
-    #         emp_id, name, url = company
-    #         for vacancy in cls.get_info_vacancies():
-    #             vac_id, vac_name, vac_url, vac_from, vac_to, vac_emp_id = vacancy
-    #             if vac_emp_id == emp_id:
-    #                 vacancies_list.append(vac_name)
-    #         vacancies_count = {f'{name}': len(vacancies_list)}
-    #         employers_list.append(vacancies_count)
-    #     return employers_list
+    @classmethod
+    def get_companies_and_vacancies_count(cls):
+        employers_list = []
+        for company in cls.get_info_employers():
+            vacancies_list = []
+            emp_id, name, url = company
+            for vacancy in cls.get_info_vacancies():
+                vac_id, vac_name, vac_url, vac_from, vac_to, vac_emp_id = vacancy
+                if vac_emp_id == emp_id:
+                    vacancies_list.append(vac_name)
+            vacancies_count = {f'{name}': len(vacancies_list)}
+            employers_list.append(vacancies_count)
+        return employers_list
 
 
 # Работодатель Skyeng id 1122462 уже существует
 # Работодатель Skillbox id 2863076 уже существует
 # Работодатель Лаборатория Касперского id 1057 уже существует
 # Работодатель Lesta Games id 856498 уже существует
-# Работодатель A&A VK id 4717868 уже существует
+# Работодатель VK id 15478 уже существует
 # Работодатель LG Electronics Inc. id 102501 уже существует
 # Работодатель SberTech id 906557 уже существует
 # Работодатель YADRO id 1993194 уже существует
@@ -54,3 +54,6 @@ class DBManager:
 # Вакансия Data Analyst id 79476191 уже существует
 
 
+db = DBManager
+data = db.get_companies_and_vacancies_count()
+print(data)

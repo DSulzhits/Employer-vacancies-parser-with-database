cf064f316030ee68cada120b05cfa771e1104dc6
DSulzhits <dsulzhits@gmail.com>
2023-04-19 14:34:27 +0300

    Добавлен sql код для создания таблицы вкансий

--- fill_DB/create_db.sql
+++ fill_DB/create_db.sql
@@ -1,6 +1,16 @@
-CREATE TABLE Employers
+CREATE TABLE employers
 (
 	employer_id int PRIMARY KEY,
 	employer_name varchar(100),
 	eployer_url varchar(200)
+);
+
+CREATE TABLE vacancies
+(
+	vacancy_id int PRIMARY KEY,
+	vacancy_name varchar(100),
+	vacancy_url varchar(200),
+	vacancy_salary_from varchar(50),
+	vacancy_salary_to varchar(50),
+	employer_id int REFERENCES employers(employer_id)
 );

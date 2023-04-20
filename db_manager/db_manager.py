08d936b92cf2188ae6d6b790fd650aad0d58d9c8
DSulzhits <dsulzhits@gmail.com>
2023-04-20 17:59:27 +0300

    Добавлен метод, get_avg_salary, get_vacancies_with_keyword,

--- db_manager/db_manager.py
+++ db_manager/db_manager.py
@@ -2,29 +2,29 @@ from fill_DB.fill_database import ConnectDB
 
 
 class DBManager:
-    @staticmethod
-    def get_info_employers():
-        conn = ConnectDB.connect_to_db()
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    cur.execute("SELECT * FROM employers")
-                    companies = cur.fetchall()
-        finally:
-            conn.close()
-        return companies
+    # @staticmethod
+    # def get_info_employers():
+    #     conn = ConnectDB.connect_to_db()
+    #     try:
+    #         with conn:
+    #             with conn.cursor() as cur:
+    #                 cur.execute("SELECT * FROM employers")
+    #                 companies = cur.fetchall()
+    #     finally:
+    #         conn.close()
+    #     return companies
 
-    @staticmethod
-    def get_info_vacancies():
-        conn = ConnectDB.connect_to_db()
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    cur.execute("SELECT * FROM vacancies")
-                    vacancies = cur.fetchall()
-        finally:
-            conn.close()
-        return vacancies
+    # @staticmethod
+    # def get_info_vacancies():
+    #     conn = ConnectDB.connect_to_db()
+    #     try:
+    #         with conn:
+    #             with conn.cursor() as cur:
+    #                 cur.execute("SELECT * FROM vacancies")
+    #                 vacancies_info = cur.fetchall()
+    #     finally:
+    #         conn.close()
+    #     return vacancies_info
 
     @classmethod
     def get_companies_and_vacancies_count(cls):
@@ -33,38 +33,64 @@ class DBManager:
         try:
             with conn:
                 with conn.cursor() as cur:
-                    cur.execute(
-                        """SELECT employer_name, COUNT(vacancies) AS vacancy_count 
-                        FROM employers 
-                        INNER JOIN vacancies 
-                        USING(employer_id) 
-                        GROUP BY employer_name 
-                        ORDER BY vacancy_count DESC""")
+                    cur.execute("""SELECT employer_name, COUNT(vacancies) AS vacancy_count 
+                                   FROM employers 
+                                   INNER JOIN vacancies 
+                                   USING(employer_id) 
+                                   GROUP BY employer_name 
+                                   ORDER BY vacancy_count DESC""")
                     emp_vac_all = cur.fetchall()
                     for emp_vac in emp_vac_all:
-                        emp, vac = emp_vac
-                        employers_vac_list.append(f"Работодатель {emp}: число вакансий {vac}")
+                        emp, vac_count = emp_vac
+                        employers_vac_list.append(f"Работодатель {emp}: число вакансий {vac_count}")
         finally:
             conn.close()
         return employers_vac_list
 
+    @classmethod
+    def get_avg_salary(cls):
+        conn = ConnectDB.connect_to_db()
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """SELECT ROUND(AVG(vacancy_salary_from))
+                           FROM vacancies
+                           WHERE vacancy_salary_from IS NOT null""")
+                    avg_salary = cur.fetchone()[0]
+        finally:
+            conn.close()
+        return avg_salary
 
-
-# Работодатель Skyeng id 1122462 уже существует
-# Работодатель Skillbox id 2863076 уже существует
-# Работодатель Лаборатория Касперского id 1057 уже существует
-# Работодатель Lesta Games id 856498 уже существует
-# Работодатель VK id 15478 уже существует
-# Работодатель LG Electronics Inc. id 102501 уже существует
-# Работодатель SberTech id 906557 уже существует
-# Работодатель YADRO id 1993194 уже существует
-# Работодатель Доктор Веб id 8884 уже существует
-# Работодатель GeekBrains id 1111672 уже существует
-# Вакансия Data Analyst id 79476191 уже существует
+    @classmethod
+    def get_vacancies_with_keyword(cls, keyword):
+        vacancies_list = []
+        conn = ConnectDB.connect_to_db()
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        f"""SELECT vacancy_name, vacancy_url, vacancy_salary_from, vacancy_salary_to FROM vacancies
+                            WHERE vacancy_name LIKE '%{keyword}%'""")
+                    vacancies_keyword = cur.fetchall()
+                    for vacancy in vacancies_keyword:
+                        name, url, sal_from, sal_to = vacancy
+                        if sal_from is None:
+                            sal_from = 'не указано'
+                        if sal_to is None:
+                            sal_to = 'не указано'
+                        vacancies_list.append(f"Вакансия: {name}, url: {url}, зарплата от: {sal_from}, до: {sal_to}")
+        finally:
+            conn.close()
+        return vacancies_list
 
 
 db = DBManager
 data = db.get_companies_and_vacancies_count()
-print(data)
 for d in data:
     print(d)
+salary = db.get_avg_salary()
+print(salary)
+vacancies = db.get_vacancies_with_keyword("Python")
+for vac in vacancies:
+    print(vac)
